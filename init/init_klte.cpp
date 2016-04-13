4de95f26e582b2ec18df5af4a279f5f66be9757f
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-04-13 05:38:19 -0600

    klte: Update SM-G900A|M|S|K fingerprint
    Change-Id: Ib0822d420449d00f032949d371153bd6d828146b

--- init/init_klte.cpp
+++ init/init_klte.cpp
@@ -72,6 +72,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.description", "klteub-user 6.0.1 MMB29M G900MUBU1CPC4 release-keys");
         property_set("ro.product.model", "SM-G900M");
         property_set("ro.product.device", "klte");
+        gsm_properties();
     } else if (strstr(bootloader, "G900A")) {
         /* klteatt */
         property_set("ro.build.fingerprint", "samsung/klteuc/klteatt:5.1.1/LMY47X/G900AUCU4CPA1:user/release-keys");
@@ -86,8 +87,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.device", "klteskt");
     } else if (strstr(bootloader, "G900K")) {
         /* kltektt */
-        property_set("ro.build.fingerprint", "samsung/kltektt/kltektt:6.0.1/MMB29M/G900KKTU1CPE1:user/release-keys");
-        property_set("ro.build.description", "kltektt-user 6.0.1 MMB29M G900KKTU1CPE1 release-keys");
+        property_set("ro.build.fingerprint", "samsung/kltektt/kltektt:6.0.1/MMB29M/G900KKTU1CPC2:user/release-keys");
+        property_set("ro.build.description", "kltektt-user 6.0.1 MMB29M G900KKTU1CPC2 release-keys");
         property_set("ro.product.model", "SM-G900K");
         property_set("ro.product.device", "kltektt");
     } else {
