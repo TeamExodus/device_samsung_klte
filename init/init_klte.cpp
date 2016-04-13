143f37d2d7682e1f73ce42fcf93a6614900b8430
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-04-13 05:38:19 -0600

    klte: Update SM-G900A|M|S|K fingerprint
    Change-Id: Ib0822d420449d00f032949d371153bd6d828146b

--- init/init_klte.cpp
+++ init/init_klte.cpp
@@ -98,8 +98,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         gsm_properties();
     } else if (strstr(bootloader, "G900K")) {
         /* kltektt */
-        property_set("ro.build.fingerprint", "samsung/kltektt/kltektt:6.0.1/MMB29M/G900KKTU1CPE1:user/release-keys");
-        property_set("ro.build.description", "kltektt-user 6.0.1 MMB29M G900KKTU1CPE1 release-keys");
+        property_set("ro.build.fingerprint", "samsung/kltektt/kltektt:6.0.1/MMB29M/G900KKTU1CPC2:user/release-keys");
+        property_set("ro.build.description", "kltektt-user 6.0.1 MMB29M G900KKTU1CPC2 release-keys");
         property_set("ro.product.model", "SM-G900K");
         property_set("ro.product.device", "kltektt");
         gsm_properties();
